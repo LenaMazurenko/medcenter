29d02f67e4e4b1983ca482b9c63690dee211281f
LenaMazurenko <lena@mazurenko.de>
2023-03-26 13:28:14 +0300

    change2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.render(
